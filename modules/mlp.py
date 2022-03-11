35e44a057ad6a4b19b6dd2154635fb326d1ebaaf
TomGoh <tom-goh@outlook.com>
2022-03-11 09:52:52 +0800

    Cross Validation
    Cross Validation on MLP
    model

--- modules/mlp.py
+++ modules/mlp.py
@@ -1,5 +1,6 @@
 from torch import nn
 
+
 def full_block(in_features, out_features, p_drop=0.0):
     return nn.Sequential(
         nn.Linear(in_features, out_features, bias=True),
@@ -8,13 +9,14 @@ def full_block(in_features, out_features, p_drop=0.0):
         nn.Dropout(p=p_drop),
     )
 
+
 class MLP(nn.Module):
 
     def __init__(self, num_genes=5000, num_hidden=128, p_drop=0.0):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Dropout(p=0.8),
-            full_block(in_features=num_genes,out_features=1024,p_drop=p_drop),
+            nn.Dropout(p=0.3),
+            full_block(in_features=num_genes, out_features=1024, p_drop=p_drop),
             full_block(in_features=1024, out_features=512, p_drop=p_drop),
             full_block(in_features=512, out_features=256, p_drop=p_drop),
             full_block(in_features=256, out_features=128, p_drop=p_drop),
@@ -22,8 +24,25 @@ class MLP(nn.Module):
         self.rep_dim = num_hidden
 
     def forward(self, x):
-
         x = self.encoder(x)
 
         return x
 
+
+class ValidMLP(nn.Module):
+
+    def __init__(self, num_genes, p_drop=0.2):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            full_block(in_features=num_genes, out_features=1024, p_drop=p_drop),
+            full_block(in_features=1024, out_features=512, p_drop=p_drop),
+            full_block(in_features=512, out_features=256, p_drop=p_drop),
+            full_block(in_features=256, out_features=128, p_drop=p_drop),
+            full_block(in_features=128, out_features=64, p_drop=p_drop),
+            nn.Linear(in_features=64, out_features=14),
+            # nn.LayerNorm(out_features),
+        )
+
+    def forward(self, x):
+        output = self.encoder(x)
+        return output
---
+++ preprocessing/transformation.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+
+
+def chiasma(original, copy):
+    pass
+
+
+def mask(original, copy):
+    pass
+
+
+def gaussian_noise(original, copy):
+    cellShape=copy.shape
+    gaussian_noise=np.random.normal(0,0.2,cellShape)
+
+
+# TODO: Refer to CLEAR to add transformation and merge into training process
